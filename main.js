c0e7a30c0c95492c79fee5a5e69a33603fb7a15a
EC2 Default User <ec2-user@ip-172-31-37-152.ec2.internal>
2023-10-25 12:51:54 +0000

    ストップウォッチ課題

---
+++ main.js
@@ -0,0 +1,74 @@
+//変数の定義
+const timer = document.getElementById('time');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+let startTime;
+let elapsedTime = 0;
+let timeoutId;
+
+//時間を取得して桁数を合わせる
+function countUp(){
+  const d = new Date(Date.now() - startTime); //現在の時刻（Date.now）からstartを押した時の時刻(startTime)を引く
+  const h = String(d.getUTCHours()).padStart(2, '0');
+  const m = String(d.getMinutes()).padStart(2, '0');
+  const s = String(d.getSeconds()).padStart(2, '0');
+  const ms = String(d.getMilliseconds()).slice(-2);
+  timer.textContent = h + ':' + m + ':' + s + ':' + ms;
+
+  timeoutId = setTimeout(() => {
+    countUp();
+  }), 100;
+}
+
+// 状態:初期 または Reset直後
+function buttonInitial() {
+  start.classList.remove('inactive'); // 活性
+  stop.classList.add('inactive')    // 非活性
+  reset.classList.add('inactive')   // 非活性
+}
+// 状態:タイマー動作中
+function buttonRunning() {
+  start.classList.add('inactive')   // 非活性
+  stop.classList.remove('inactive');  // 活性
+  reset.classList.add('inactive')   // 非活性
+}
+// 状態:タイマー停止中
+function buttonStopped() {
+  start.classList.remove('inactive'); // 活性
+  stop.classList.add('inactive')    // 非活性
+  reset.classList.remove('inactive'); // 活性
+}
+// ボタンを'初期'状態とする
+buttonInitial()
+
+// Startボタンクリック
+start.addEventListener('click', () => {
+  if (start.classList.contains('inactive') === true) {
+    return;
+  }
+  buttonRunning();
+  startTime = Date.now();
+  countUp();
+});
+
+//Stopボタンクリック
+stop.addEventListener('click', () => {
+  if (stop.classList.contains('inactive') === true) {
+    return;
+  }
+  buttonStopped();
+  clearTimeout(timeoutId);
+  elapsedTime += Date.now() - startTime;
+});
+
+//リセットボタンクリック
+reset.addEventListener('click', () => {
+  if (reset.classList.contains('inactive') === true) {
+    return;
+  }
+  buttonInitial()
+  timer.textContent = '00:00:00:00';
+  elapsedTime = 0;
+});
