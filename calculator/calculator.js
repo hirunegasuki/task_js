9754ce365122a1173cbf4e6ac5089114c7d6ffba
EC2 Default User <ec2-user@ip-172-31-37-152.ec2.internal>
2023-11-02 13:04:45 +0000

    電卓課題

---
+++ calculator/calculator.js
@@ -0,0 +1,39 @@
+const button = document.querySelectorAll('button');
+const result = document.getElementById('result');
+
+let calcText = "";
+
+function buttonClick(event) {
+  const text = event.target.textContent;
+
+  if(text === "=") {
+    calcText = eval(calcText);
+  } 
+  else if(text === "AC") {
+    calcText = "";
+  }
+  else {
+    calcText = calcText += text;
+  }
+  result.textContent = calcText;
+}
+
+function symbol(target) {
+  //var last = result.textContent.slice(-1);　//末尾の文字列を取得
+  //var first = result.textContent.slice(0, 1);　//先頭の文字列を取得
+  var last = calcText.slice(-1);　//末尾の文字列を取得
+  var first = calcText.slice(0, 1);　//先頭の文字列を取得
+  
+  if(last === "+" || last === "-" || last === "*" || last === "/"){
+    target.value.slice(0, -1);
+    result.textContent += target.value;
+
+  //if (last === "+" || last === "-" || last === "*" || last === "/"){
+    //let str = result.textContent.slice(0, -1);
+    //result.textContent += target.value;
+  }
+}
+//*/
+
+
+button.forEach(button => button.addEventListener('click', buttonClick));
