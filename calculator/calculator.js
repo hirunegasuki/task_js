99ca3bfb7bf4b627c3698b8067fa258f9e26d947
EC2 Default User <ec2-user@ip-172-31-37-152.ec2.internal>
2023-11-03 09:28:24 +0000

    電卓課題

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,6 +1,9 @@
 const button = document.querySelectorAll('button');
 const result = document.getElementById('result');
 
+var last = result.textContent.slice(-1);　//末尾の文字列を取得
+var first = result.textContent.slice(0, 1);　//先頭の文字列を取得
+
 let calcText = "";
 
 function buttonClick(event) {
@@ -12,20 +15,27 @@ function buttonClick(event) {
   else if(text === "AC") {
     calcText = "";
   }
+  //else if(text === "+" || text === "-" || text === "*" || text === "/") {
+  else if(text === symbol){
+    //calcText += symbol;
+    return;
+  }
   else {
     calcText = calcText += text;
   }
   result.textContent = calcText;
 }
 
-function symbol(target) {
-  var last = result.textContent.slice(-1);　//末尾の文字列を取得
-  var first = result.textContent.slice(0, 1);　//先頭の文字列を取得
-  
-  if (last === "+" || last === "-" || last === "*" || last === "/"){
+function symbol(event) {
+  if (last === "+"||"-"||"*"||"/"){
     let str = result.textContent.slice(0, -1);
-    result.textContent += target.value;
-    console.log(str)
+    result.textContent = str + event.textContent;
+  }
+  else{
+    result.textContent += event.textContent;
+    ////let str = result.textContent.slice(0, -1);
+    ////result.textContent += event.target.textContent;
+    //console.log(event.textContent);
    }
 }
 //*/
